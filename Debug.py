0eb1f5fed3311e054bd6aa187964ad60d8dbba99
244267620@qq.com <wlc68911qet>
2019-06-04 19:38:27 +0800

    update

--- Debug.py
+++ Debug.py
@@ -9,6 +9,7 @@ import gc
 from EntityRelation.GeographicalDomain.China.CityCrawler import *
 from EntityRelation.GeographicalDomain.China.AirportCrawler import *
 from EntityRelation.GeographicalDomain.China.TrainStationCrawler import *
+from EntityRelation.GeographicalDomain.China.RegionCrawler import *
 from EntityInformation.BaiduEncyclopedia import *
 from Neo4j.Neo4j import *
 
@@ -115,6 +116,32 @@ def cnt_save () -> None:
 	neo4j = Neo4j(ip=conn['ip'], password=conn['password'])
 	neo4j.add_graph(entity_info, entity_rel)
 
+
+def cnr_entity_extract () -> None:
+	"""
+	中国区域实体信息及关系抽取
+	:return: None
+	"""
+	entity_info_list, entity_rel = get_region()
+	for entity_info in entity_info_list:
+		entity_info_extract(entity_info['property'])
+
+	with open('./CacheData/GeographicalDomain/China/Region/EntityInfo.json', 'w', encoding='utf-8') as file_object:
+		json.dump(entity_info_list, file_object, ensure_ascii=False, indent=2)
+	with open('./CacheData/GeographicalDomain/China/Region/EntityRel.json', 'w', encoding='utf-8') as file_object:
+		json.dump(entity_rel, file_object, ensure_ascii=False, indent=2)
+
+
+def cnr_save () -> None:
+	with open('./conn_info.json', 'r', encoding='utf-8') as file_object:
+		conn = json.load(file_object)
+	with open('./CacheData/GeographicalDomain/China/Region/EntityInfo.json', 'r', encoding='utf-8') as file_object:
+		entity_info = json.load(file_object)
+	with open('./CacheData/GeographicalDomain/China/Region/EntityRel.json', 'r', encoding='utf-8') as file_object:
+		entity_rel = json.load(file_object)
+
+	neo4j = Neo4j(ip=conn['ip'], password=conn['password'])
+	neo4j.add_graph(entity_info, entity_rel)
+
 if __name__ == '__main__':
-	cna_save()
-	cnt_save()
+	cnr_entity_extract()
